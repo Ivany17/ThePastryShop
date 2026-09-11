66a8231ddebb7cfe6707e99c5feeff94411291c1
Ivany17 <yaremich98@gmail.com>
2026-09-11 19:36:11 +0300

    Add CORS, render product cards from API data with structured HTML

--- frontend/app.js
+++ frontend/app.js
@@ -3,11 +3,17 @@ const productsContainer = document.getElementById('products-container');
 async function loadProducts() {
     const getResponse = await fetch("http://localhost:5160/api/products");
     const data = await getResponse.json();
-    console.log(data);
+    const productsHTML = data.map((p) => {
+        return `
+            <div class="product-card">
+                <h3>${p.name}</h3>
+                <p>${p.description}</p>
+                <p>${p.price} ₴</p>
+                <button class="addBtn">Add</button>
+            </div>
+        `;
+    }).join("");
+    productsContainer.innerHTML = productsHTML;
 }
 
 loadProducts();
-
-let arr = [1, 2, 3];
-let newArr = arr.map(n => n * 2);
-console.log(newArr);
